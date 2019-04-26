6f3afbcc37312b11d78e5d4fab34c74ca03a9ef9
brampauwelyn <brampauw@gmail.com>
2019-04-26 13:46:43 +0200

    quick change for testing

--- server/app.py
+++ server/app.py
@@ -20,7 +20,11 @@ def changeLedColor():
   red = int(request.args.get('r')) / 255
   green = int(request.args.get('g')) / 255
   blue = int(request.args.get('b')) / 255
+  while True:
+    led.color(red,green,blue)
+    sleep(0.1)
   return "red: {}, green:  {}, blue: {}".format(red,green,blue)
+
   
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
