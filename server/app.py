4764f08cb9e7ff83227473bf677c007d7a9e9849
brampauwelyn <brampauw@gmail.com>
2019-04-26 14:22:14 +0200

    set sleep to 1s

--- server/app.py
+++ server/app.py
@@ -20,10 +20,9 @@ def changeLedColor():
   red = int(request.args.get('r')) / 255
   green = int(request.args.get('g')) / 255
   blue = int(request.args.get('b')) / 255
-  while True:
-    led.color = (red,green,blue)
-    sleep(0.1)
-    return "OK"
+  led.color = (red,green,blue)
+  sleep(1)
+  return "OK"
   
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
