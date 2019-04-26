4fd26e6f949b4388186f32355e992e2fb56f0f53
brampauwelyn <brampauw@gmail.com>
2019-04-26 14:46:35 +0200

    fix syntax error

--- server/app.py
+++ server/app.py
@@ -22,7 +22,7 @@ def changeLedColor():
   green = int(request.args.get('g')) / 255
   blue = int(request.args.get('b')) / 255
   led.color = (red,green,blue)
-  led.blink(on_time=1, off_time=1, fade_in_time=0, fade_out_time=0, on_color=(red, green, blue), off_color=(red, green, blue), n=None, background=True
+  led.blink(on_time=1, off_time=1, fade_in_time=0, fade_out_time=0, on_color=(red, green, blue), off_color=(red, green, blue), n=None, background=True)
   return "OK"
   
 if __name__ == "__main__":
