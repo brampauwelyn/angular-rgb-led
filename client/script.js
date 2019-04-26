032864fd3d97f5f9c7024bc57e0832945f25d607
brampauwelyn <brampauw@gmail.com>
2019-04-26 13:20:46 +0200

    first commit

---
+++ client/script.js
@@ -0,0 +1,29 @@
+(function(){
+
+  var app = angular.module("ledApp", []);
+
+  var ledController = function($scope,$http){
+
+    var ipaddress = "localhost:5000";
+
+    $scope.red = 0;
+    $scope.green = 0;
+    $scope.blue = 0;
+
+    var changeLedColor = function(){
+      var cssLedLight = document.getElementsByClassName('led-light')[0];
+      cssLedLight.style.backgroundColor = `rgb(${$scope.red},${$scope.green},${$scope.blue})`;
+      var url = `http://${ipaddress}/led?r=${$scope.red}&g=${$scope.green}&b=${$scope.blue}`;
+      $http.get(url)
+        .then(console.log(res));
+    }
+
+
+    $scope.changeLedColor = changeLedColor;
+
+  }
+
+
+  app.controller('ledController', ["$scope","$http", ledController]);
+
+}());
---
+++ server/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from gpiozero import RGBLED
+from time import sleep
+
+app = Flask(__name__)
+
+# define the GPIO pins for the led lights
+redpin = 14
+greenpin = 18
+bluepin = 15
+
+led = RGBLED(redpin, greenpin, bluepin)
+
+@app.route('/hello')
+def hello():
+  return "world"
+
+@app.route('/led', methods = ['GET'])
+def changeLedColor():
+  red = int(request.args.get('r')) / 255
+  green = int(request.args.get('g')) / 255
+  blue = int(request.args.get('b')) / 255
+  while True:
+    led.color(red,green,blue)
+    sleep(0.1)
+    return "red: {}, green:  {}, blue: {}".format(red,green,blue)
+  
+if __name__ == "__main__":
+  app.run(debug=True)
